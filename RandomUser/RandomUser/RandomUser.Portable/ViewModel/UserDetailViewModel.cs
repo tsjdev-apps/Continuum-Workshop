caea74a43cf5f82348697b0a3466de71324043a3
Sebastian Jensen <sebastian@tsjdev-apps.de>
2016-09-25 00:31:08 +0200

    - add viewmodels

---
+++ RandomUser/RandomUser/RandomUser.Portable/ViewModel/UserDetailViewModel.cs
@@ -0,0 +1,23 @@
+﻿using RandomUser.Core.Interfaces.Repository;
+using RandomUser.Core.Model;
+using RandomUser.Portable.Interfaces.Repository;
+using RandomUser.Portable.Model;
+using RandomUser.Portable.ViewModel;
+
+namespace RandomUser.Core.ViewModel
+{
+    public class UserDetailViewModel : AsyncViewModelBase
+    {
+        private User _user;
+        public User User
+        {
+            get { return _user; }
+            set { _user = value; RaisePropertyChanged(); }
+        }
+        
+        public UserDetailViewModel(IUserRepository userRepository)
+        {
+            userRepository.SelectedUserChanged += (__, user) => User = user;
+        }
+    }
+}
