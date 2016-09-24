f8760afbfd568bc87252082fa957f773a3246d99
Sebastian Jensen <sebastian@tsjdev-apps.de>
2016-09-25 00:25:43 +0200

    - add model

---
+++ RandomUser/RandomUser/RandomUser.Portable/Model/User.cs
@@ -0,0 +1,14 @@
+﻿namespace RandomUser.Portable.Model
+{
+    public class User
+    {
+        public string Gender { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Cell { get; set; }
+        public string PictureUrl { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
