5b3ccc581d7ae901aa6f9967b7458d554a83d80f
Sebastian Jensen <sebastian@tsjdev-apps.de>
2016-09-25 00:28:50 +0200

    - add interfaces

---
+++ RandomUser/RandomUser/RandomUser.Portable/Interfaces/Service/IDataService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using RandomUser.Portable.Model;
+using RandomUser.Portable.Utils;
+
+namespace RandomUser.Portable.Interfaces.Service
+{
+    public interface IDataService
+    {
+        Task<DataServiceResponse<IEnumerable<User>>> GetUsersAsync(int count);
+    }
+}
